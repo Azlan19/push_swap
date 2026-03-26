407f7c300f8bae29910d717e3d5cabec62182112
Azlan19 <omarazlan6@gmail.com>
2026-03-26 15:42:36 +0100

    (PLAN) Comment push_swap plan

--- main.c
+++ main.c
@@ -6,9 +6,22 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 14:46:34 by oazlan            #+#    #+#             */
-/*   Updated: 2026/03/26 14:50:58 by oazlan           ###   ########.fr       */
+/*   Updated: 2026/03/26 15:42:06 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// PLAN
+// TURK ALGORITHM
+// 1) If stack 'a' > 3
+//      - Push 'a' node to stack 'b'
+//          - While pushing 'a' node to 'b', sort 'b' in descending order
+// 2) Once stack 'a' = 3
+//      - Sort the 3 nodes in stack 'a'
+// 3) Push all 'b' nodes back to stack 'a'
+
+// (Have target nodes from a->b or b->a)
+// (closest bigger/smaller value before pushing from 1 stack to the other)
+
 
 #include <stdio.h>
 
