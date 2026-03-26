552d426b505bba46ec24fc83432e4941aaef09b4
Azlan19 <omarazlan6@gmail.com>
2026-03-26 16:43:43 +0100

    (HEADER FILE) Create include folder and add header file

---
+++ include/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 16:37:25 by oazlan            #+#    #+#             */
+/*   Updated: 2026/03/26 16:42:23 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+
+# include "../ft_printf/include/ft_printf.h"
+# include <stdbool.h>
+# include <limits.h>
+
+
+# endif
