f3606b22ee5dee5a404aabf3cee41e7b2c519c8d
Azlan19 <omarazlan6@gmail.com>
2026-03-26 16:49:26 +0100

    (ADD) Define struct for each node

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,17 +6,28 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 16:37:25 by oazlan            #+#    #+#             */
-/*   Updated: 2026/03/26 16:42:23 by oazlan           ###   ########.fr       */
+/*   Updated: 2026/03/26 16:48:19 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-
 # include "../ft_printf/include/ft_printf.h"
-# include <stdbool.h>
 # include <limits.h>
+# include <stdbool.h>
 
+typedef struct s_stack_node
+{
+	int					value;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
 
-# endif
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+#endif
