acbc7136b53307414d807d31a086debe70351930
Omar Azlan <oazlan@2-C-7.42heilbronn.de>
2026-04-14 18:16:55 +0200

    (ADD FUNC) Complete logic with swap command

--- func_swap.c
+++ func_swap.c
@@ -6,9 +6,23 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 17:55:55 by oazlan            #+#    #+#             */
-/*   Updated: 2026/04/14 17:56:22 by oazlan           ###   ########.fr       */
+/*   Updated: 2026/04/14 18:14:36 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "include/push_swap.h"
 
+void swap(t_stack** stack)
+{
+    t_stack* first;
+    t_stack* second;
+
+    first = *stack;
+    second = first->next;
+    first->next = second->next;
+    second->next = first;
+    first->prev = second;
+    second->prev = NULL;
+    *stack = second;
+    
+}
