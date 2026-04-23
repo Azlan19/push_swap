f1acbabc32fa0e4f5a829bd6d5c49cf22d3ecaa1
Azlan19 <omarazlan6@gmail.com>
2026-04-23 17:49:16 +0200

    (ADD FUNC) Add tiny sort for sorting 3 numbers

---
+++ sort_three.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 17:04:00 by oazlan            #+#    #+#             */
+/*   Updated: 2026/04/23 17:47:37 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/push_swap.h"
+
+void sort_three(t_stack** stack)
+{
+    t_stack* biggest_node;
+
+    biggest_node = find_max(*stack);
+
+    if (biggest_node == *stack)
+    {
+        ra(stack);
+    }
+    else if (biggest_node == (*stack)->next)
+    {
+        rra(stack);
+    }
+    if ((*stack)->value > (*stack)->next->value)
+    {
+        sa(stack);
+    }
+}
